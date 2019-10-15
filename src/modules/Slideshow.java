b4b18889dde7f78e3d1cfe48419e9557ee5c3882
ColeChittim <chittic3@wwu.edu>
2019-10-14 17:53:43 -0700

    Started skeleton and updated makefile

---
+++ src/modules/Slideshow.java
@@ -0,0 +1,20 @@
+package modules;
+
+class Slideshow extends Module{
+    int imageCount;
+    int imageIndex;
+    int secondsPerImage;
+
+    //Slideshow view refrence
+
+    Slideshow(){
+        //Displays slideshow module view and controller
+    }
+    
+    public void PlaySlideshow(){}
+    public void PauseSlideshow(){}
+    
+    void Update(){
+    }
+
+}
