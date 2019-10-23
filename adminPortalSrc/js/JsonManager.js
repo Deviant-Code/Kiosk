96c7df735cd01a2e32acf398f12307b5d2bf461a
ricenad <ricenad@wwu.edu>
2019-10-22 19:53:47 -0700

    updated server code and jsonmanager

--- adminPortalSrc/js/JsonManager.js
+++ adminPortalSrc/js/JsonManager.js
@@ -1,3 +1,7 @@
+'use strict';
+
+
+
 function updateJson() {
     var d = new Date();
     var n = d.getTime();
@@ -5,10 +9,10 @@ function updateJson() {
     alert("Changing Text to " + n);
     var jsonString = JSON.stringify(text);
 
+
     //Write jsonString to file will require something like node.js
     //But is very common and may already be installed in labs
     //Here's vaguely what you'd use
-
     /*
     var fs = require("fs");
     fs.writeFile("./public/settings.json", jsonString, (err) => {
@@ -19,4 +23,23 @@ function updateJson() {
         console.log("File has been created");
     });
     */
+
+
+    
 }
+
+function getJson(){
+    
+    //CODE FOR GETTING JSON BELOW!
+    //load fs module to app
+    const fs = require('fs');
+    //get relative path
+    let rawdata = fs.readFileSync('../public/settings.json');
+    //parse using json
+    var object = JSON.parse(rawdata);
+    //console.log(object);
+    var objectString = JSON.stringify(object);
+
+    alert("got object:" + objectString);
+}
+
--- adminPortalSrc/js/server.js
+++ adminPortalSrc/js/server.js
@@ -1,7 +1,50 @@
+/*README
+ start the server from the command line in the js folder. 
+ start using node server.js. it will display a server in the command line, go there.
+ you will need to change the directory to the location of the index.html in your folder.
+
+*/
+
+
+
+//CODE FOR GETTING JSON BELOW!
+//load fs module to app
+/*const fs = require('fs');
+//get relative path
+let rawdata = fs.readFileSync('../public/settings.json');
+//parse using json
+var object = JSON.parse(rawdata);
+console.log(object);
+var objectString = JSON.stringify(object);
+*/
+
+
+
+
+
+//CODE FOR SERVER BELOW!
+
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, "127.0.0.1");
+var fs = require('fs');
+
+
+
+
+var http = require('http');
+var server = http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});//html so its not treated as text
+    //HARD CODED IN DIRECTORY. CHANGE TO YOUR DIRECTORY FOR IT TO WORK.
+    var myReadStream = fs.createReadStream('/home/ricenad/Desktop/492/kiosk/adminPortalSrc/index.html', 'utf8');
+    //pipe to html
+    myReadStream.pipe(res);
+    //res.end('Hello World\n');
+    //res.end(objectString);
+});
+//listen on port and ip
+server.listen(3000, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:3000/');
+//alert("server started");
+
+
