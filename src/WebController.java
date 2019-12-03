045afe5ce017a36df799d66ebe5735ced0d4796e
Jesse <ericksenjesse@gmail.com>
2019-12-02 23:04:32 -0800

    Updates to poll

--- src/WebController.java
+++ src/WebController.java
@@ -1,8 +1,11 @@
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,5 +24,13 @@ public class WebController implements Initializable {
 
     public void setMenuScene(Scene menuScene) {
         this.menuScene = menuScene;
+    }
+
+    public void openMenuScene(Event actionEvent) {
+        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        //Weird JavaFX bug requires toggle on full screen after swapping scenes
+        //primaryStage.setFullScreen(false);
+        primaryStage.setScene(menuScene);
+        primaryStage.setFullScreen(true);
     }
 }
