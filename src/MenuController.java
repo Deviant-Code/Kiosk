60f71df13011c79d00825072e3c315bba0e50659
Jesse <ericksenjesse@gmail.com>
2019-12-04 03:50:57 -0800

    Large Commit. Major changes to transitions fixing the latency/resizing issue that has been bugging me since the beginning of time. FXML updates, touch gesture updates, slideshow updates

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,68 +1,55 @@
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import manager.KioskManager;
-import javafx.scene.Parent;
 import modules.Slideshow;
-import modules.Video;
 
 import java.io.IOException;
 
 public class MenuController {
 
-    private Scene ssScene;
     private SlideshowController ssController;
-    private Scene webScene;
-
-    public void setSlideshowScene(Scene ssScene) {
-        this.ssScene = ssScene;
-    }
+    private Parent slideshowRoot;
+    private Scene scene;
+    private Parent pollViewRoot;
+    private Parent webViewRoot;
 
     @FXML
     public void openSlideShow(Event actionEvent) throws IOException {
 
         Slideshow ss = KioskManager.getSS();
-        ss.updateSlides();
-        ssController.setSlideshow(ss);
-
-        ImageView imageView = (ImageView) ssScene.lookup("#ss_image_view");
+        ImageView imageView = (ImageView) slideshowRoot.lookup("#ss_image_view");
         Image image = new Image(ss.getImage("CURRENT"));
         imageView.setImage(image);
 
-        //Set Stage to slideshow
-        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-
-        //Weird JavaFX bug requires toggle on full screen after swapping scenes
-        //primaryStage.setFullScreen(false);
-        primaryStage.setScene(ssScene);
-        primaryStage.setFullScreen(true);
-
+        scene.setRoot(slideshowRoot);
         ss.setTimer();
-
     }
 
-    public void openVideo(Event actionEvent) throws IOException {
-
-        Video video = KioskManager.getVideo();
-
+    public void openPolls(Event actionEvent) throws IOException {
+        scene.setRoot(pollViewRoot);
     }
 
-
-    public void setSlideshowController(SlideshowController controller) {
-        this.ssController = controller;
+    public void openDepartment(Event actionEvent) throws IOException {
+        scene.setRoot(webViewRoot);
     }
 
-    public void openWebView(Event actionEvent) throws IOException {
-        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        primaryStage.setScene(webScene);
-        primaryStage.setFullScreen(true);
+    public void setSlideshowRoot(Parent slideshow) {
+        this.slideshowRoot = slideshow;
     }
 
-    public void setWebScene(Scene webScene) {
-        this.webScene = webScene;
+    public void setScene(Scene menuScene) {
+        this.scene = menuScene;
+    }
+
+    public void setPollRoot(Parent pollViewRoot) {
+        this.pollViewRoot = pollViewRoot;
+    }
+
+    public void setWebRoot(Parent webViewRoot) {
+        this.webViewRoot = webViewRoot;
     }
 }
--- src/WebController.java
+++ src/WebController.java
@@ -1,36 +1,35 @@
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.stage.Stage;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WebController implements Initializable {
     @FXML
     private WebView viewweb;
-    private Scene menuScene;
+    private Scene scene;
+    private Parent menuRoot;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         final WebEngine web = viewweb.getEngine();
-        String urlweb = "http://127.0.0.1:3000/pages/kioskPoll.html";
+        String urlweb = "https://cse.wwu.edu/computer-science";
         web.load(urlweb);
     }
 
-    public void setMenuScene(Scene menuScene) {
-        this.menuScene = menuScene;
+    public void openMenuScene(Event actionEvent) {
+        scene.setRoot(menuRoot);
     }
 
-    public void openMenuScene(Event actionEvent) {
-        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        //Weird JavaFX bug requires toggle on full screen after swapping scenes
-        //primaryStage.setFullScreen(false);
-        primaryStage.setScene(menuScene);
-        primaryStage.setFullScreen(true);
+    public void setMenuRoot(Parent menuRoot) {
+        this.menuRoot = menuRoot;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
     }
 }
